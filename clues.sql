debe7719ace90a5e7de1e00b0137f1b66dde261e
Kier Roman <kierroman@gmail.com>
2025-05-21 20:13:05 -0500

    solved previous issues with incorrect pulls

--- clues.sql
+++ clues.sql
@@ -1,8 +1,8 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
 -- Write SQL query here
-SELECT * 
-FROM countries 
+SELECT c.name 
+FROM countries c
 WHERE region = 'Southern Europe' 
 ORDER BY population ASC 
 LIMIT 1;
@@ -22,9 +22,8 @@ WHERE c.name = 'Holy See (Vatican City State)' AND cl.isofficial = TRUE;
 SELECT c.name
 FROM countries c
 JOIN countrylanguages cl ON c.code = cl.countrycode
-WHERE cl.language = 'Italian'
-GROUP BY c.code, c.name
-HAVING COUNT(*) = 1;
+WHERE cl.language = 'Italian' AND cl.percentage = 100;
+
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
@@ -32,7 +31,7 @@ HAVING COUNT(*) = 1;
 SELECT ci.name
 FROM cities ci
 JOIN countries co ON ci.countrycode = co.code
-WHERE co.name = 'Holy See (Vatican City State)' AND ci.name != 'Holy See (Vatican City State)';
+WHERE co.name = 'San Marino' AND ci.name != 'San Marino';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
@@ -41,8 +40,8 @@ SELECT ci.name AS city, co.name AS country
 FROM cities ci
 JOIN countries co ON ci.countrycode = co.code
 WHERE co.region = 'South America'
-  AND ci.name ILIKE '%ano' 
-  AND ci.name NOT ILIKE '%Vaticano%';
+  AND ci.name ILIKE '%serra%' 
+  AND ci.name NOT ILIKE '%valle';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
@@ -52,7 +51,7 @@ WHERE co.region = 'South America'
 SELECT ci.name AS capital
 FROM countries co
 JOIN cities ci ON co.capital = ci.id
-WHERE co.name = 'Chile';
+WHERE co.name = 'Brazil';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
